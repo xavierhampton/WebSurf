be0756611ae8a6bd8def2c8dbd8df5c078831a84
Xavier Hampton <xaviermichaelhampton@gmail.com>
2025-03-12 23:20:45 -0500

    organized important three and cannon methods, comments

--- src/client/client.ts
+++ src/client/client.ts
@@ -4,19 +4,23 @@ import * as CANNON from 'cannon-es'
 import pMove from './components/pMove';
 import Stats from 'three/examples/jsm/libs/stats.module';
 
+//Initialize Global Objects
 const stats = new Stats()
 const pmove = new pMove()
-document.body.appendChild(stats.dom)
 
-const spaceTexture = new THREE.TextureLoader().load('assets/spaceTexture.png')
-spaceTexture.wrapS = THREE.RepeatWrapping
-spaceTexture.wrapT = THREE.RepeatWrapping
-spaceTexture.repeat.set(2,2)
+//Initalize Global Variables
+let meshes : any = []
+const clock = new THREE.Clock()
 
+//////////////////////////////////
+//Initialize Three.js Scene
 const scene = new THREE.Scene();
-scene.background = spaceTexture
 
+//Initialize Cannon.js World
+const world = new CANNON.World();
+world.gravity.set(0, -20, 0);
 
+//Initialize Camera
 const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
@@ -25,23 +29,56 @@ const camera = new THREE.PerspectiveCamera(
 )
 camera.position.set(0,2,5)
 
-//Mesh List
-let meshes : any = []
+//Initialize Renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true
 
-window.addEventListener('resize', onWindowResize, false);
-function onWindowResize() {
+/////////////////////////////////
+//Initalize Controls
+const controls = new PointerLockControls(camera, renderer.domElement)
+controls.pointerSpeed = 0.5
+
+//Player
+const player = new THREE.Mesh(new THREE.BoxGeometry(1,3,1), new THREE.MeshLambertMaterial({color: 0x005555}))
+player.position.y = 2
+player.castShadow = true
+scene.add(player)
+
+const playerShape = new CANNON.Box(new CANNON.Vec3(0.5, 1.5, 0.5))
+const playerBody = new CANNON.Body({mass: 12, material: new CANNON.Material({friction: 0})})
+playerBody.addShape(playerShape)
+playerBody.position.set(
+    player.position.x,
+    player.position.y,
+    player.position.z
+)
+world.addBody(playerBody)
+
+//Set Scene Background
+const spaceTexture = new THREE.TextureLoader().load('assets/spaceTexture.png')
+spaceTexture.wrapS = THREE.RepeatWrapping
+spaceTexture.wrapT = THREE.RepeatWrapping
+spaceTexture.repeat.set(2,2)
+scene.background = spaceTexture
+
+// Lighting
+const light = new THREE.DirectionalLight(0xffffff, Math.PI)
+light.position.x = 25
+light.position.y = 25
+light.castShadow = true
+light.shadow.camera.scale.x = light.shadow.camera.scale.y = 10
+const ambLight = new THREE.AmbientLight(0xffffff, .5)
+light.shadow.mapSize.width = light.shadow.mapSize.height = 1024
+scene.add(light, ambLight)
+
+//Browser Events
+window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight)
     render();
-}
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.shadowMap.enabled = true
-document.body.appendChild(renderer.domElement);
-const controls = new PointerLockControls(camera, renderer.domElement)
-controls.pointerSpeed = 0.5
+}, false);
 
 const menuPanel = document.getElementById("menuPanel") as HTMLDivElement
 const startButton = document.getElementById("startButton") as HTMLInputElement
@@ -53,28 +90,12 @@ controls.addEventListener('change', () => {console.log('controls change')})
 controls.addEventListener('lock', () => {menuPanel.style.display = 'none'})
 controls.addEventListener('unlock', () => {menuPanel.style.display = 'block'})
 
+//Insert DOM Elements
+document.body.appendChild(stats.dom)
+document.body.appendChild(renderer.domElement);
 
-// Lighting
-const light = new THREE.DirectionalLight(0xffffff, Math.PI)
-light.position.x = 25
-light.position.y = 25
-light.castShadow = true
-light.shadow.camera.scale.x = light.shadow.camera.scale.y = 10
-
-const ambLight = new THREE.AmbientLight(0xffffff, .5)
-
-
-//How pixelated shadows are smaller = more
-light.shadow.mapSize.width = light.shadow.mapSize.height = 1024
-scene.add(light, ambLight)
-
-
-//Physics
-
-const world = new CANNON.World();
-world.gravity.set(0, -20, 0);
-const clock = new THREE.Clock()
-
+////////////////////////////
+////////////////////////////
 
 //Plane
 const planeMesh = new THREE.Mesh(
@@ -91,7 +112,6 @@ const planeBody = new CANNON.Body({mass: 0, material: new CANNON.Material({frict
 planeBody.addShape(planeShape)
 planeBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1,0,0), -Math.PI / 2)
 world.addBody(planeBody)
-
 
 
 const cubeTexture = new THREE.TextureLoader().load('./assets/cookieTexture.png')
@@ -143,25 +163,9 @@ for (let i = 0; i < 100; i++) {
     world.addBody(cubeBody)
 }
 
-//Player
 
-const player = new THREE.Mesh(new THREE.BoxGeometry(1,3,1), new THREE.MeshLambertMaterial({color: 0x005555}))
-player.position.y = 2
-player.castShadow = true
-scene.add(player)
-
-const playerShape = new CANNON.Box(new CANNON.Vec3(0.5, 1.5, 0.5))
-const playerBody = new CANNON.Body({mass: 12, material: new CANNON.Material({friction: 0})})
-playerBody.addShape(playerShape)
-playerBody.position.set(
-    player.position.x,
-    player.position.y,
-    player.position.z
-)
-world.addBody(playerBody)
 
 //Movement
-
 let moveForward = false;
 let moveBackward = false;
 let moveLeft = false;
@@ -209,7 +213,6 @@ const onKeyUp = (event: KeyboardEvent) => {
 
 document.addEventListener('keydown', onKeyDown, false)
 document.addEventListener('keyup', onKeyUp, false)
-
 
 
 function move(delta: number) {
